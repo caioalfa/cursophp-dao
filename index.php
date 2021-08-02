2c9b128a4ce34807d0675c73dd8fd19c267d04e6
unknown <k_io19@hotmail.com>
2021-08-02 19:25:50 -0300

    criada a classe usuarios

--- index.php
+++ index.php
@@ -2,10 +2,21 @@
 
     require_once("config.php");
 
+    /* Traz do banco os usuarios
+
     $sql = new Sql();
 
     $usuarios = $sql->select("SELECT * FROM tb_usuarios");
     
     echo json_encode($usuarios);
+    */
+    $usuario = new Usuario();
+     
+    $usuario->loadById(8);
+
+    echo $usuario;
+
+
+
 
 ?>
